505759738a1bc1823fcdc95c614c930c0cd384e7
hyeonpearl <hc9064@gmail.com>
2022-12-08 23:07:56 +0900

    Programmers Beginner Day 2

---
+++ beginner/day2/day2_3.js
@@ -0,0 +1,12 @@
+function solution(denum1, num1, denum2, num2) {
+  let denum = denum1 * num2 + denum2 * num1;
+  let num = num1 * num2;
+  let lcm = 1;
+
+  for (let i = 1; i <= denum; i++) {
+    if (denum % i === 0 && num % i === 0) {
+      lcm = i;
+    }
+  }
+  return [denum / lcm, num / lcm];
+}
---
+++ beginner/day2/day2_4.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+  let answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    answer[i] = numbers[i] * 2;
+  }
+  return answer;
+}
