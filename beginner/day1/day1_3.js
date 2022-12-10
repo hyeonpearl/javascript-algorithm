f2bd22b1dac2bfba6c94452d93acbeea7e839aae
hyeonpearl <hc9064@gmail.com>
2022-12-10 13:56:54 +0900

    Day 1 화살표 함수로 수정

--- beginner/day1/day1_3.js
+++ beginner/day1/day1_3.js
@@ -1,3 +1,1 @@
-function solution(num1, num2) {
-  return num1 * num2;
-}
+const solution = (num1, num2) => num1 * num2;
--- beginner/day1/day1_4.js
+++ beginner/day1/day1_4.js
@@ -1,3 +1,1 @@
-function solution(num1, num2) {
-  return Math.floor(num1 / num2);
-}
+const solution = (num1, num2) => Math.floor(num1 / num2);
