af02211fb9af80267fef8ee2be4f47f82c13464f
hyeonpearl <hc9064@gmail.com>
2023-04-26 15:17:37 +0900

    Programmers Lv.0 문제 풀이

---
+++ programmers/level_0/컨트롤_제트.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  return s
+    .split(' ')
+    .reduce(
+      (sum, cur, index, arr) =>
+        cur === 'Z' ? sum - arr[index - 1] : sum + Number(cur),
+      0
+    );
+}
---
+++ programmers/level_0/한_번만_등장한_문자.js
@@ -0,0 +1,38 @@
+// 다른 사람 풀이
+function solution(s) {
+  const count = [...s].reduce(
+    (a, c) => (a[c] ? { ...a, [c]: a[c] + 1 } : { ...a, [c]: 1 }),
+    {}
+  );
+
+  return Object.keys(count)
+    .filter(key => count[key] === 1)
+    .sort((a, b) => (a < b ? -1 : a > b ? 1 : 0))
+    .join('');
+}
+
+function solution(s) {
+  let res = [];
+
+  for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+
+  return res.sort().join('');
+}
+
+var solution = s =>
+  [...s]
+    .filter(c => s.match(new RegExp(c, 'g')).length == 1)
+    .sort()
+    .join('');
+
+function solution(s) {
+  const m = new Map();
+
+  [...s].forEach(e => m.set(e, m.get(e) + 1 || 1));
+
+  return [...m]
+    .filter(e => e[1] === 1)
+    .map(e => e[0])
+    .sort()
+    .join('');
+}
