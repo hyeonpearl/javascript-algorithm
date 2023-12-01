0586069650a53fd973ee7bf3b5c2d20af3b9517c
hyeonpearl <hc9064@gmail.com>
2023-12-01 18:55:07 +0900

    [PGS] 최솟값 만들기 / Lv.2

---
+++ programmers/Lv_2/최솟값_만들기.js
@@ -0,0 +1,13 @@
+// 정답 1
+function solution(A, B) {
+  let answer = 0;
+
+  A = A.sort((a, b) => a - b);
+  B = B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[i];
+  }
+
+  return answer;
+}
