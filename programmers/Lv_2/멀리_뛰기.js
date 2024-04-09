f5623ecf2b713b6a0d082bfe743340baa2fd253e
hyeonpearl <hyeoncheol.contact@gmail.com>
2024-04-09 20:01:25 +0900

    [PGS] 멀리 뛰기 / Lv.2

---
+++ programmers/Lv_2/멀리_뛰기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  if (n < 2) return 1;
+
+  const count = [0, 1, 2, ...Array(n - 2).fill(0)];
+
+  count.forEach((_, i) => {
+    if (i > 2) count[i] = (count[i - 2] + count[i - 1]) % 1234567;
+  });
+
+  return count[n];
+}
