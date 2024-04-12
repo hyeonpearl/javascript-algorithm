3d2552ace43c3aeb2cf431d837125a64634b686a
hyeonpearl <hyeoncheol.contact@gmail.com>
2024-04-12 23:48:44 +0900

    [PGS] 카펫 / Lv.2

---
+++ programmers/Lv_2/카펫.js
@@ -0,0 +1,16 @@
+function solution(brown, yellow) {
+  const answer = [];
+  let sum = brown + yellow;
+
+  for (let height = 3; height < brown / 2; height++) {
+    if (sum % height === 0) {
+      let weight = sum / height;
+
+      if ((height - 2) * (weight - 2) === yellow) {
+        return [weight, height];
+      }
+    }
+  }
+
+  return answer;
+}
